f0216228e18df0a5417c46f932f0b8d02018cfaa
mostafa <mostafa@civilsoft.net>
2023-09-14 14:37:09 +0300

    fixing base URL

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     //<React.StrictMode>
     <Provider store={store}>
-        <BrowserRouter basename={"/hcm"}>
+        <BrowserRouter basename={"https://mostafaelhussainy.github.io/hcm/"}>
             <App />
         </BrowserRouter>
     </Provider>
